731633429edae2934a88059b0b34674e247121a7
Abid Talukdar <abiditalukdar@gmail.com>
2020-01-28 21:12:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,16 @@ class Dog
 end
  
 fido = Dog.new
+<<<<<<< HEAD
 snoopy = Dog.new 
 lassie = Dog.new
 
+=======
+fido #=> #<Dog:0x007fc52c2d7d20>
+ 
+snoopy = Dog.new
+snoopy #=> #<Dog:0x007fc52c2d4170>
+ 
+lassie = Dog.new
+lassie #=> #<Dog:0x007fc52c2cc588>
+>>>>>>> 476dccd62854ad5d7da6507510b5eb4cc5d3c68b
